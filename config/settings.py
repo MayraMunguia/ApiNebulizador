1862438123cbece81598df97018a12389d00a77c
root <mayrahopem@gmail.com>
2021-04-07 00:22:21 -0700

    uwu

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = conf['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
